5e5f12ec737ed6d59ff4b13a16f1f4c167c3ecdb
Mac Ignacio <mack_ignacio@yahoo.com>
2020-06-20 18:08:19 +0800

    CHORE: Replace plexer by native duplex stream

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import { optionBuilder, transformStream } from "./utils";
 import { ElasticBeanstalkDeployOptions } from "./interface";
+import { Duplex } from "stream";
 import EB from "./eb";
 import S3 from "./s3";
 import waiton from "./wait";
 import deploy from "./deploy";
+import plexer from "./duplex";
 import zip from "gulp-zip";
 import through from "through2";
-
-const plexer = require("plexer");
 
 /**
  * Gulp plugin for deploying environment into AWS elastic beanstalk.
  * @params options
  */
-export = function (options: ElasticBeanstalkDeployOptions) {
+export = function (options: ElasticBeanstalkDeployOptions): Duplex {
   options = optionBuilder(options);
   const zipper = zip(options.filename);
   const streamPipe = zipper.pipe(through.obj(transformStream(options, S3, EB, deploy, waiton)));
